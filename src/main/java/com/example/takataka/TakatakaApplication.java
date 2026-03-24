1709917311568c8757e9ba4508a21f6802ee194f
takai_ryuta <takai.ryuta@alhinc.jp>
2026-03-24 11:26:20 +0900

    First Commit

---
+++ src/main/java/com/example/takataka/TakatakaApplication.java
@@ -0,0 +1,13 @@
+package com.example.takataka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakatakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakatakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/takataka/TakatakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takataka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakatakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
